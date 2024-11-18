b1067115a30fdc095bd44d55972b4c38673a5179
Mo'Khaled Bin-Hazem <Mo7ammedkhal3d@gmail.com>
2024-11-18 23:23:14 +0300

    setup UI and requiered resources

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React,{Fragment} from "react";
+import mealsImage from '../../assets/images/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = props =>{
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>Mael Order</h1>
+                <button>
+                    Cart
+                </button>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt='A table full of delicious food!'/>
+            </div>
+        </Fragment>
+    );
+}
+
+export default Header;
---
+++ src/components/UI/HeaderCartButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from './HeaderCartButton.module.css';
+
+const HeaderCartButton = props =>{
+    return(
+        <button>
+            <span>
+                
+            </span>
+            <span></span>
+            <span></span>
+        </button>
+    );
+}
+
+export default HeaderCartButton;
