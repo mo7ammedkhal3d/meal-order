2f1132e35a51ad8d930a73014b4b7b1f9e35942d
Mo'Khaled Bin-Hazem <Mo7ammedkhal3d@gmail.com>
2024-11-20 00:23:23 +0300

    start with components logic

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,20 +1,19 @@
 import { Fragment } from 'react';
-import classes from './MealItemForm.module.css'
 import Input from '../../UI/Input';
+import classes from './MealItemForm.module.css'
 const MealItemForm = props =>{
     return(
         <Fragment>
             <form className={classes.form}>
                 <Input
                     label="Amount" input={{
-                        id:'amount',
-                        type:'number',
-                        min:'1',
-                        max:'5',
-                        step:'1',
-                        defaultValue:'1',}
-                    }/>
-
+                    id:'amount',
+                    type:'number',
+                    min:'1',
+                    max:'5',
+                    step:'1',
+                    defaultValue:'1',}
+                }/>
                 <button>+ Add</button>
             </form>
         </Fragment>
